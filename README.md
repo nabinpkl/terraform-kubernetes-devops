05abb860da7a7d52778214952c7606f3a732963e
nabinpkl <nabin62246@gmail.com>
2024-10-09 10:44:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Scalable Web Application with CI/CD Pipeline
+
+This repostiory contains simple web application built with node.js which is then deployed using CI/CD pipeline by using contenrization.
+
+
+### Requiremnts
+Node >= 18
+
+
+### How to run locally?
+```
+node server.js
+```
+Access the website at [localhost:8080](localhost:8080)
+
---
+++ server.js
@@ -0,0 +1,30 @@
+// Author: Nabin Pokhrel C0904908
+
+express = require('express');
+const path = require('path');
+
+app = express();
+
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle requests for the home page if not found then send error page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'), (err) => {
+        if (err) {
+            res.status(err.status || 404).sendFile(path.join(__dirname, 'public', 'error.html'));
+        }
+    });
+});
+
+
+// Handle 404 errors for static files
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', 'error.html'));
+});
+
+//Start the server on port 8080
+app.listen(8080, () => {
+    console.log("Server is listenning on port 8080");
+});
